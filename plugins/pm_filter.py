ec43fd49d27d8082ce17903105c2afe8a850fc19
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2022-07-10 17:49:52 +0600

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -349,7 +349,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f_caption
             buttons = [
             [
-                InlineKeyboardButton('🌀 Jᴏɪɴ Fᴏʀ Mᴏʀᴇ 🌀', url='https://t.me/Cinemaathattakam_Links')
+                InlineKeyboardButton('🌀 Jᴏɪɴ Fᴏʀ Mᴏʀᴇ 🌀', url='https://t.me/CT_Arena')
             ]
             ]
         if f_caption is None:
