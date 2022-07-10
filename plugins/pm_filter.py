1b3cce154efd2065848df7532262661f189d05a1
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2022-07-10 17:36:49 +0600

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -440,7 +440,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('ᴄᴛ ʟɪɴᴋs', url='https://t.me/Cinemaathattakam_Links'),
+            InlineKeyboardButton('ᴄᴛ ʟɪɴᴋs', url='https://t.me/CT_Arena'),
             InlineKeyboardButton('ᴍᴏᴠɪᴇs', url='https://t.me/joinchat/AxHu-k3fvsZmMDE1'),
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='start')
         ]]
